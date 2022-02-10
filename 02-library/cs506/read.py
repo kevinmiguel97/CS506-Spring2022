6c05b2f6b6648fca0bc903be993efe70cd9fbdef
Kevin Figueroa <kevmiguelarronte@gmail.com>
2022-02-09 20:07:39 -0500

    m

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -10,14 +10,6 @@ def read_csv(csv_file_path):
 
     #raise NotImplementedError()
 
-#matrix = read_csv('Matrix.csv', decode='utf-8')
 
-import csv
-rows = []
-with open("Matrix.csv", 'r) as file:
-    csvreader = csv.reader(file)
-    header = next(csvreader)
-    for row in csvreader:
-        rows.append(row)
-print(header)
-print(rows)
+
+
